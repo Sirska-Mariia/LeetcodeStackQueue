6cd0ad5966ff56707eccead0c9e9b46806b30538
Sirska-Mariia <149333906+Sirska-Mariia@users.noreply.github.com>
2024-04-16 20:06:04 +0300

    pylint for Implement Stack using Queues fixed

--- Implement Stack using Queues/Implement Stack using Queues.py
+++ Implement Stack using Queues/Implement Stack using Queues.py
@@ -1,54 +1,83 @@
+"""
+stack using queue
+"""
+
 class Queue:
+    """
+    queue
+    """
     def __init__(self):
         self.lt = []
-        
 
     def push(self, x: int) -> None:
-      self.lt.append(x)  
+        """
+        adds new element
+        """
+        self.lt.append(x)
 
     def pop(self) -> int:
+        """
+        deletes first element
+        """
         if len(self.lt)>0:
             return self.lt.pop(0)
         return -1
 
     def top(self) -> int:
+        """
+        returns first element
+        """
         if len(self.lt)>0:
             return self.lt[0]
         return -1
-        
+
     def empty(self) -> bool:
+        """
+        checks if empty
+        """
         return len(self.lt)==0
 
 class MyStack:
-
+    """
+    stack
+    """
     def __init__(self):
         self.st = Queue()
         self.other = Queue()
-        
+
 
     def push(self, x: int) -> None:
+        """
+        adds new element
+        """
         self.st.push(x)
 
     def pop(self) -> int:
+        """
+        deletes new element
+        """
         while not self.st.empty():
-                s = self.st.pop()
-                self.other.push(s)
+            s = self.st.pop()
+            self.other.push(s)
         while not self.other.empty():
-                m = self.other.pop()
-                if m != s:
-                    self.st.push(m)
+            m = self.other.pop()
+            if m != s:
+                self.st.push(m)
         return s
-        
 
     def top(self) -> int:
+        """
+        returns last element
+        """
         while not self.st.empty():
-                self.other.push(self.st.pop())
+            self.other.push(self.st.pop())
         while not self.other.empty():
-                m = self.other.pop()
-                self.st.push(m)
+            m = self.other.pop()
+            self.st.push(m)
         return m
-        
 
     def empty(self) -> bool:
+        """
+        checks if stack is empty
+        """
         return self.st.empty()
-        
