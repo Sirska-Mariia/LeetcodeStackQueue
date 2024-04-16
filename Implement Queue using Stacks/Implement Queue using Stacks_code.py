ca0540f3d7152ddf4c8aea2c3bd10f686c458bf5
Sirska-Mariia <149333906+Sirska-Mariia@users.noreply.github.com>
2024-04-16 20:14:05 +0300

    fixed pylint for implement Queue using Stacks

--- Implement Queue using Stacks/Implement Queue using Stacks_code.py
+++ Implement Queue using Stacks/Implement Queue using Stacks_code.py
@@ -1,38 +1,60 @@
+"""
+queue using stacks
+"""
 class Stack():
+    """
+    stack
+    """
     def __init__(self):
-        self.st = [] 
-        
+        self.st = []
 
     def push(self, x: int) -> None:
+        """
+        adds new element
+        """
         self.st.append(x)
-        
 
     def pop(self) -> int:
+        """
+        deletes last element
+        """
         if len(self.st)>0:
             return self.st.pop()
         return -1
-        
 
     def peek(self) -> int:
+        """
+        returns last element
+        """
         if len(self.st)>0:
             return self.st[-1]
         return -1
-        
 
     def empty(self) -> bool:
+        """
+        checks if empty
+        """
         return len(self.st) == 0
 
 
 class MyQueue:
+    """
+    queue
+    """
     def __init__(self):
         self.lt = Stack()
         self.other = Stack()
 
     def push(self, x: int) -> None:
+        """
+        Pushes element x to the back of the queue.
+        """
         self.lt.push(x)
-        
 
     def pop(self) -> int:
+        """
+        Removes the element from the front of the queue and returns it
+        """
         if not self.lt.empty():
             while not self.lt.empty():
                 self.other.push(self.lt.pop())
@@ -41,9 +63,11 @@ class MyQueue:
                 self.lt.push(self.other.pop())
             return s
         return -1
-        
 
     def peek(self) -> int:
+        """
+        Returns the element at the front of the queue
+        """
         if not self.lt.empty():
             while not self.lt.empty():
                 self.other.push(self.lt.pop())
@@ -52,8 +76,10 @@ class MyQueue:
             while not self.other.empty():
                 self.lt.push(self.other.pop())
             return s
-        
+        return -1
 
     def empty(self) -> bool:
+        """
+        Returns true if the queue is empty, false otherwise
+        """
         return self.lt.empty()
-        
